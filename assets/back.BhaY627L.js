9ef55edaede0f5cd83ce7dbafe7abca4c2d24136
smdzhm <smdzhm@users.noreply.github.com>
2025-03-27 12:52:11 +0000

    deploy: d1e09f87a14b17418013b3694a83039ecc33af22

---
+++ assets/back.BhaY627L.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.Dem9hqPK.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
